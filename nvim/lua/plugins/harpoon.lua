988f4c1b692bed6469d0ae2e7f03f0b77a7ff39c
bertdev <herbertmatos10@hotmail.com>
2023-12-21 21:46:31 -0300

    refactor: change harpoon keymappings

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
         vim.keymap.set("n", "<leader>ho", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<leader>h", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>j", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<leader>k", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<leader>l", function() harpoon:list():select(4) end)
