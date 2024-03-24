b265f385d351f5fbd53897509c148dc9a9c7ed11
bertdev <herbertmatos10@hotmail.com>
2024-03-23 22:03:14 -0300

    feat(neovim): add keymaps to jump to diagnostics and clear search highlighting

--- nvim/lua/vim-base.lua
+++ nvim/lua/vim-base.lua
@@ -6,7 +6,22 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set cursorline")
 vim.g.mapleader = " "
+
+-- sync os and neovim clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- navigation
 vim.keymap.set('n', '<C-h>', '<C-w>h', {})
 vim.keymap.set('n', '<C-l>', '<C-w>l', {})
 vim.keymap.set('n', '<C-k>', '<C-w>k', {})
 vim.keymap.set('n', '<C-j>', '<C-w>j', {})
+
+-- clear search higlight using esc
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- diagnostic jump keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
