14087701c7088665f1e3e66a8f5e9a390c917b48
bertdev <herbertmatos10@hotmail.com>
2024-03-23 23:08:56 -0300

    feat(neovim): add more keymaps to lsp configs

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -58,13 +58,34 @@ return {
             lspconfig.jsonls.setup({})
             lspconfig.marksman.setup({})
 
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+                callback = function (event)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+                    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references)
+                    vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations)
+                    vim.keymap.set('n', '<leader>D', require('telescope.builtin').lsp_type_definitions)
+                    vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols)
+                    vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_workspace_symbols)
+                    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {})
-            vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, {})
-            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+                    -- higlight references when hold cursor on something
+                    local client = vim.lsp.get_client_by_id(event.data.client_id)
+                    if client and client.server_capabilities.documentHighlightProvider then
+                        vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                            buffer = event.buf,
+                            callback = vim.lsp.buf.document_highlight
+                        })
+
+                        vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                            buffer = event.buf,
+                            callback = vim.lsp.buf.clear_references
+                        })
+                    end
+                end
+            })
         end
     }
 }
