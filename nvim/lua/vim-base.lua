6ee41ae954e4ac707ab892c4040f4e535e05029c
bertdev <herbertmatos10@hotmail.com>
2024-03-23 23:08:14 -0300

    feat(neovim): add autocommand to highlight when yank text

--- nvim/lua/vim-base.lua
+++ nvim/lua/vim-base.lua
@@ -25,3 +25,11 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
+-- higlighting when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'higlight when yanking text',
+    group = vim.api.nvim_create_augroup('higlight-yank', { clear = true }),
+    callback = function ()
+        vim.highlight.on_yank()
+    end
+})
