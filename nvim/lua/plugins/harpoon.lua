ffe3441129eb9c506d8a748b4f5cdf11f4b19d5b
bertdev <herbertmatos10@hotmail.com>
2024-03-23 21:51:49 -0300

    fix(neovim): fix harpoon setup call

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { {"nvim-lua/plenary.nvim"} },
     config = function()
         local harpoon = require("harpoon")
-        harpoon:setup()
+        harpoon:setup({})
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
         vim.keymap.set("n", "<leader>ho", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
