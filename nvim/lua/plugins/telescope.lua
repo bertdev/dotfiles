b6d28dea6309debf30cfad98e560eeb4bc59714c
bertdev <herbertmatos10@hotmail.com>
2024-03-23 23:08:40 -0300

    feat(neovim): add more keymaps to telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,10 +4,25 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>sk', builtin.keymaps, {})
+            vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
+
+            -- search on current file
+            vim.keymap.set('n', '<leader>s/', function ()
+                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                    winblend = 10,
+                    previewer = false,
+                })
+            end)
+
+            -- search on neovim config files
+            vim.keymap.set('n', '<leader>sn', function ()
+                builtin.find_files { cwd = vim.fn.stdpath 'config' }
+            end)
         end
     },
     {
